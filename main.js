2bac8d5ad3dac23f9247afb4e52ef3b4a4763633
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-01 18:51:03 +0100

    feat || secondo esercizio

--- main.js
+++ main.js
@@ -2,27 +2,51 @@
 
 //creare una funzione con una stringa come parametro.
 
-function reverseWord (text) {
-    let reversedText = '';
+//function reverseWord (text) {
+//    let reversedText = '';
 //creare un ciclo for, per ottenere la stringa girata.
-    for (let i = text.length - 1; i >= 0; i--){
-        reversedText += text[i];
-    }
-    return reversedText;
-}
+//    for (let i = text.length - 1; i >= 0; i--){
+//        reversedText += text[i];
+//    }
+//    return reversedText;
+//}
 //Crea un event listener
-window.addEventListener('load', function(){
+//window.addEventListener('load', function(){
 
     //creare un prompt per chiedere all'utente la parola che desidera girare.
-    let inputUtente = prompt('Dimmi che parola vuoi girare');
+//    let inputUtente = prompt('Dimmi che parola vuoi girare');
     //mostrare la versione girata, grazie alla funzione creata in precedenza e fare un innertext del paragrafo con ID, per mostrare il risultato all'utente.
-    let word = reverseWord(inputUtente);
+//    let word = reverseWord(inputUtente);
+//    let paragrafo = document.getElementById('risultato');
+//    paragrafo.innerText = `Il risultato della parola girata da te scelta è: ${word}`
+
+//})
+
+
+//Crea una funzione che concatena una lista di stringhe. Chiedi all'utente quante stringhe vuole concatenare e poi mostragli il risultato.
+
+//crea una funzione con parametro un array
+function concatenetedString (stringList) {
+    let concatenate = '';
+    for (let i = 0; i < stringList.length; i++) {
+        let string = stringList[i];
+        concatenate += string;
+    }
+    return concatenate;
+}
+
+
+window.addEventListener('load', function(){
+
+    let inputUtente = prompt('Quante stringhe vuoi concatenare?');
+    let strings = [];
+    for(let i = 0; i < inputUtente; i++){
+        let thisString = prompt (`Dammi la stringa ${i + 1}`)
+        strings [i] = thisString;
+    }
+    let concatenate = concatenetedString (strings);
     let paragrafo = document.getElementById('risultato');
-    paragrafo.innerText = `Il risultato della parola girata da te scelta è: ${word}`
-    
+    paragrafo.innerText = `La concatenazione delle tue stringhe è: ${concatenate}`
+
 })
-
-
-
-
 
